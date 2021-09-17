3cd3c7a28362605e22359f3618e9054e452910a8
curriecoder <andrew.j.currie0@gmail.com>
2021-09-17 15:20:27 -0400

    finished setting up local storage for events

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,88 +11,98 @@ $("#currentDay").text(today);
 // I want to compare the time-block hour to the current hour
 // change the color of the textarea for past, present and future
 
-
-
-if (9 < (currentHour)) {
-  $("#09").addClass("past")
-} else if (9 > (currentHour)) {
-  $("#09").addClass("future")
+if (9 < currentHour) {
+  $("#9").addClass("past");
+} else if (9 > currentHour) {
+  $("#9").addClass("future");
 } else {
-  $("#09").addClass("present")
-};
-
-if (10 < (currentHour)) {
-  $("#10").addClass("past")
-} else if (10 > (currentHour)) {
-  $("#10").addClass("future")
-} else {
-  $("#10").addClass("present")
-};
-
-if (11 < (currentHour)) {
-  $("#11").addClass("past")
-} else if (11 > (currentHour)) {
-  $("#11").addClass("future")
-} else {
-  $("#11").addClass("present")
-};
-
-if (moment(12).isBefore(currentHour)) {
-  $("#12").addClass("past")
-} else if (moment(12).isAfter(currentHour)) {
-  $("#12").addClass("future")
-} else {
-  $("#12").addClass("present")
-};
-
-if (moment(13).isBefore(currentHour)) {
-  $("#13").addClass("past")
-} else if (moment(13).isAfter(currentHour)) {
-  $("#13").addClass("future")
-} else {
-  $("#13").addClass("present")
-};
-
-if (moment(14).isBefore(currentHour)) {
-  $("#14").addClass("past")
-} else if (moment(14).isAfter(currentHour)) {
-  $("#14").addClass("future")
-} else {
-  $("#14").addClass("present")
-};
-
-if (moment(15).isBefore(currentHour)) {
-  $("#15").addClass("past")
-} else if (moment(15).isAfter(currentHour)) {
-  $("#15").addClass("future")
-} else {
-  $("#15").addClass("present")
-};
-
-if (moment(16).isBefore(currentHour)) {
-  $("#16").addClass("past")
-} else if (moment(16).isAfter(currentHour)) {
-  $("#16").addClass("future")
-} else {
-  $("#16").addClass("present")
-};
-
-if (moment(17).isBefore(currentHour)) {
-  $("#17").addClass("past")
-} else if (moment(17).isAfter(currentHour)) {
-  $("#17").addClass("future")
-} else {
-  $("#17").addClass("present")
-};
-
-// When save button is clicked, store description text in local storage.
-function save(event) {
-  var description = $("textarea").val();
-  var saveBtn = $(event.target);
-  
-  console.log(description);
+  $("#9").addClass("present");
 }
 
-var container = $(".container")
+if (10 < currentHour) {
+  $("#10").addClass("past");
+} else if (10 > currentHour) {
+  $("#10").addClass("future");
+} else {
+  $("#10").addClass("present");
+}
+
+if (11 < currentHour) {
+  $("#11").addClass("past");
+} else if (11 > currentHour) {
+  $("#11").addClass("future");
+} else {
+  $("#11").addClass("present");
+}
+
+if (12 < currentHour) {
+  $("#12").addClass("past");
+} else if (12 > currentHour) {
+  $("#12").addClass("future");
+} else {
+  $("#12").addClass("present");
+}
+
+if (13 < currentHour) {
+  $("#13").addClass("past");
+} else if (13 > currentHour) {
+  $("#13").addClass("future");
+} else {
+  $("#13").addClass("present");
+}
+
+if (14 < currentHour) {
+  $("#14").addClass("past");
+} else if (14 > currentHour) {
+  $("#14").addClass("future");
+} else {
+  $("#14").addClass("present");
+}
+
+if (15 < currentHour) {
+  $("#15").addClass("past");
+} else if (15 > currentHour) {
+  $("#15").addClass("future");
+} else {
+  $("#15").addClass("present");
+}
+
+if (16 < currentHour) {
+  $("#16").addClass("past");
+} else if (16 > currentHour) {
+  $("#16").addClass("future");
+} else {
+  $("#16").addClass("present");
+}
+
+if (17 < currentHour) {
+  $("#17").addClass("past");
+} else if (17 > currentHour) {
+  $("#17").addClass("future");
+} else {
+  $("#17").addClass("present");
+}
+// When save button is clicked, store description text in local storage.
+
+renderLast();
+
+function renderLast() {
+  for (var i = 9; i < 18; i++) {
+    localStorage.getItem(i);
+    $("#" + i).val(localStorage.getItem(i));
+  }
+}
+
+function save(event) {
+  event.preventDefault();
+  var saveBtn = $(event.target);
+  var description = saveBtn.parent().siblings("textarea").val();
+  var numId = saveBtn.parent().siblings("textarea").attr("id");
+
+  localStorage.setItem(numId, description);
+  console.log(saveBtn.parent().siblings("textarea").val());
+}
+
+var container = $(".container");
 
 container.on("click", ".saveBtn", save);
