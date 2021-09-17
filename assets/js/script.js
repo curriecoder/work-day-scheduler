c8bf5fd77fa03b7bd0c5d6777af7595a88135797
curriecoder <andrew.j.currie0@gmail.com>
2021-09-17 04:39:34 -0400

    hour block bgcolor changing works as intended

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var today = moment().format("[Today is ]MMM Do, YYYY");
-// var currentHour = moment().hour();
-var currentHour = 10;
+var currentHour = moment().hour();
+// var timeBlockHr = $(".textarea").value;
+// var timeBlockArr = [];
+
+// console.log(timeBlockHr);
 
 $("#currentDay").text(today);
 
@@ -8,25 +11,25 @@ $("#currentDay").text(today);
 // I want to compare the time-block hour to the current hour
 // change the color of the textarea for past, present and future
 
-if (moment(9).isBefore(currentHour)) {
+if (9 < (currentHour)) {
   $("#09").addClass("past")
-} else if (moment(9).isAfter(currentHour)) {
+} else if (9 > (currentHour)) {
   $("#09").addClass("future")
 } else {
   $("#09").addClass("present")
 };
 
-if (moment(10).isBefore(currentHour)) {
+if (10 < (currentHour)) {
   $("#10").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (10 > (currentHour)) {
   $("#10").addClass("future")
 } else {
   $("#10").addClass("present")
 };
 
-if (moment(11).isBefore(currentHour)) {
+if (11 < (currentHour)) {
   $("#11").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (11 > (currentHour)) {
   $("#11").addClass("future")
 } else {
   $("#11").addClass("present")
@@ -34,7 +37,7 @@ if (moment(11).isBefore(currentHour)) {
 
 if (moment(12).isBefore(currentHour)) {
   $("#12").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(12).isAfter(currentHour)) {
   $("#12").addClass("future")
 } else {
   $("#12").addClass("present")
@@ -42,7 +45,7 @@ if (moment(12).isBefore(currentHour)) {
 
 if (moment(13).isBefore(currentHour)) {
   $("#13").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(13).isAfter(currentHour)) {
   $("#13").addClass("future")
 } else {
   $("#13").addClass("present")
@@ -50,7 +53,7 @@ if (moment(13).isBefore(currentHour)) {
 
 if (moment(14).isBefore(currentHour)) {
   $("#14").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(14).isAfter(currentHour)) {
   $("#14").addClass("future")
 } else {
   $("#14").addClass("present")
@@ -58,7 +61,7 @@ if (moment(14).isBefore(currentHour)) {
 
 if (moment(15).isBefore(currentHour)) {
   $("#15").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(15).isAfter(currentHour)) {
   $("#15").addClass("future")
 } else {
   $("#15").addClass("present")
@@ -66,7 +69,7 @@ if (moment(15).isBefore(currentHour)) {
 
 if (moment(16).isBefore(currentHour)) {
   $("#16").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(16).isAfter(currentHour)) {
   $("#16").addClass("future")
 } else {
   $("#16").addClass("present")
@@ -74,7 +77,7 @@ if (moment(16).isBefore(currentHour)) {
 
 if (moment(17).isBefore(currentHour)) {
   $("#17").addClass("past")
-} else if (moment(10).isAfter(currentHour)) {
+} else if (moment(17).isAfter(currentHour)) {
   $("#17").addClass("future")
 } else {
   $("#17").addClass("present")
